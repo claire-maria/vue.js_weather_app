e0c08752b1205e371abe41335a3e612353bba80d
claire <cgribbin@tcd.ie>
2019-10-01 13:03:43 +0100

    f

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="app">
+    <h1>VueJS: Weather</h1>
+
+    <weather></weather>
+
+</div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      city: "",
+      submitted: false,
+      weather: {}
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
