415a6750d9c8df7cae5f44d90ab6f673c41ae8ee
claire <cgribbin@tcd.ie>
2019-10-02 13:09:17 +0100

    'getWeather'

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
 <div id="app">
     <h1>VueJS: Weather</h1>
-
+    <input type="text" placeholder="City" v-model="city">
+    <form @submit.prevent="getWeather">
+      <input type="text" placeholder="City" v-model="city">
+    </form>
     <weather></weather>
+         <div v-if="submitted">
+          <h2>{{ weather.name }} Weather</h2>
+          <div class="description">{{ weather.description }}</div>
+          <div class="temp">It's currently {{ weather.temp }}° F.</div>
+          <div class="highlow">High: {{ weather.high }}° F. Low: {{ weather.low }}° F.</div>
+      </div>
 
 </div>
 
@@ -19,6 +28,30 @@ export default {
       submitted: false,
       weather: {}
     };
+  }
+  methods: {
+    getWeather(){
+      this.weather = {}
+      this.submitted = false
+      fetch(
+        'http://api.openweathermap.org/data/2.5/find?q=${
+          this.city
+        }&units=imperial&appid=6b3705bb90e87433d0711ed38d1e6062'
+      )
+       .then(response => response.json())
+        .then(response => {
+          let data = response.list[0];
+          this.submitted = true;
+          this.weather.name = data.name;
+          this.weather.description = data.weather[0].main;
+          this.weather.temp = Math.round(data.main.temp);
+          this.weather.high = Math.round(data.main.temp_max);
+          this.weather.low = Math.round(data.main.temp_min);
+        })
+        .catch(error => console.log(error));
+      this.city = "";
+    }
+    }
   }
 }
 </script>
